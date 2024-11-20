c09ef3a835de20699d48b72e1b4038cbe93a479a
subhadeep-wisho <subhadeep@wisho.app>
2024-11-20 20:06:13 +0530

    added

--- src/main/java/org/estore/e_store_order_service/controller/TestController.java
+++ src/main/java/org/estore/e_store_order_service/controller/TestController.java
@@ -13,39 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequiredArgsConstructor
 public class TestController {
 
-    private final AuthService authService;
-    private final ProductService productService;
-    private final PaymentService paymentService;
-    private final RazorpayExternalClient razorpayExternalClient;
-
-    @GetMapping("/user")
-    public ResponseEntity<?> getUser(
-            @RequestHeader("Authorization") String authHeader) {
-        return new ResponseEntity<>(authService.getUserFromHeader(authHeader), HttpStatus.OK);
+    @GetMapping("/test")
+    public String test(){
+        return "Working fine";
     }
-
-    @GetMapping("/fetch")
-    public ResponseEntity<?> getProduct(
-            @RequestParam("pId") String productId) {
-        return new ResponseEntity<>(productService.getProductByProuctId(productId), HttpStatus.OK);
-    }
-
-    @GetMapping("/create-order")
-    public ResponseEntity<?> testPayment(
-            @RequestParam("amount") double amount
-    ){
-        return new ResponseEntity<>(paymentService.getOrderTxnDetails(amount),HttpStatus.OK);
-    }
-
-    @GetMapping("/get-txn/{txnId}")
-    public ResponseEntity<?> getTxn(@PathVariable String txnId){
-        
-        return new ResponseEntity<>(razorpayExternalClient.getTransactionDetails(txnId),HttpStatus.OK);
-    }
+   
 }
